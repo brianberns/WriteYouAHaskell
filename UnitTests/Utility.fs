935851db11fba9a5377d9d55c6fc7a0c797d9665
Brian Berns <brianberns@outlook.com>
2022-11-20 16:35:56 -0500

    Organized.

---
+++ UnitTests/Utility.fs
@@ -0,0 +1,11 @@
+namespace WriteYou
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+type Assert private () =
+
+    // Improves error message for F# types (e.g. discriminated unions).
+    static member AreEqual<'t when 't : equality>(expected : 't, actual : 't) =
+        if actual <> expected then
+            sprintf "\nExpected: %A.\nActual:   %A" expected actual
+                |> Assert.Fail
